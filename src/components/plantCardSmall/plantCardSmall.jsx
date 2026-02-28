f25eb561e1ff75e1739e220cb9265c27ad7a6843
Marjolijn-v <mverspiek@gmail.com>
2026-02-28 14:13:56 +0100

    plantCardSmall - CSS toegevoegd

--- src/components/plantCardSmall/plantCardSmall.jsx
+++ src/components/plantCardSmall/plantCardSmall.jsx
@@ -10,28 +10,32 @@ function PlantCardSmall( {plantName, plantDescription, location }) {
     const navigate = useNavigate();
 
     return(
-        <article className="small-card-container">
-            <div className="small-card-visual">
-                <div className="circle large"></div>
-                <div className="circle small"></div>
-                <img src={cactus} alt="cactus" className="plant-image-small"/>
-            </div>
-            <h2 className="small-card-title">{plantName}</h2>
-            <p className="small-card-description">{plantDescription}</p>
-            <div className="small-card-location-button">
+        <section className="small-card-outer-container">
+            <article className="small-card-container">
+                <div className="small-card-visual">
+                    <div className="circle small"></div>
+                    <div className="circle large"></div>
+                    <img src={cactus} alt="cactus" className="plant-image-small"/>
+                </div>
+                <div className="small-card-inner-container">
+                    <h2 className="small-card-title">{plantName}</h2>
+                    <p className="small-card-description">{plantDescription}</p>
+                    <div className="small-card-location-button">
                 <span className="location-wrapper">
                     <p>Location:</p>
                     <p>{location}</p>
                 </span>
-                <Button
-                    className="button-on-card"
-                    type="button"
-                    title="Switch!"
-                    onClick={() => navigate('/plant')}
-                />
-            </div>
+                        <Button
+                            className="button-on-card"
+                            type="button"
+                            title="Switch!"
+                            onClick={() => navigate('/plant')}
+                        />
+                    </div>
+                </div>
 
-        </article>
+            </article>
+        </section>
     );
 }
 
