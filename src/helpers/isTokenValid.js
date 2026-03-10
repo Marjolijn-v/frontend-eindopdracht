40f9fc3b753e755235c73a98159982fcf22220ea
Marjolijn-v <mverspiek@gmail.com>
2026-03-10 11:55:52 +0100

    Poging implementeren zoekfunctie op Search-pagina

--- src/helpers/isTokenValid.js
+++ src/helpers/isTokenValid.js
@@ -1,5 +1,15 @@
 function isTokenValid(decodedToken) {
-    return true;
+
+    const expiration = decodedToken.exp;
+    const currentTime = Date.now()/1000;
+
+    if (currentTime < expiration) {
+        return true;
+    } else {
+        return false;
+    }
+
+
 
 }
 
--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,11 +1,69 @@
 import './Search.css'
-import React from 'react';
+import React, {useState} from 'react';
 import Button from "../../components/button/button.jsx";
 import PlantCardSmall from "../../components/plantCardSmall/plantCardSmall.jsx";
+import axios from "axios";
 
 function Search() {
+
+    const [error, toggleError] = useState(false);
+    const [loading, toggleLoading] = useState(false);
+
+    const [searchResult, setSearchResult] = useState();
+    const [inputValue, setInputValue] = useState('');
+
+    async function searchPlants () {
+        try {
+            toggleLoading(true);
+            toggleError(false);
+
+            const result = await axios.get(`https://novi-backend-api-wgsgz.ondigitalocean.app/api/plants/${inputValue.toLowerCase()}`, {
+                headers: {
+                    'novi-education-project-id': '2767c1c3-13ff-45b7-a2b7-6870077651b3'
+                },
+            });
+
+            console.log(result.data);
+            console.log(result.data[0].namePlant);
+            console.log(result.data[0].description);
+
+
+            setSearchResult(result.data);
+            setInputValue('');
+
+
+        } catch (error) {
+            console.error(error);
+            toggleError(true);
+        } finally {
+            toggleLoading(false);
+        }
+    }
+
+
+
     return(
         <>
+        <div>
+            <input type="text"
+                   name="search"
+                   id="search-field"
+                   value={inputValue}
+                   onChange={(e) => setInputValue(e.target.value)}
+                   onKeyDown={(e) => e.key === "Enter" && searchPlants()}
+            />
+
+            <button type="button" onClick={searchPlants} disabled={loading} >Search</button>
+
+            {error && <p>Plant not found. Please try again.</p>}
+
+        </div>
+
+            <div>
+                <h2>{searchResult?.namePlant}</h2>
+                <p>{searchResult?.description}</p>
+            </div>
+
             <div className="outer-container search-result">
                 <header className="header search-result">
                     <h2>Search results</h2>
